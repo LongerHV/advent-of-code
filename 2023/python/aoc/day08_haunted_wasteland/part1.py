50b2eae0d8e84c85677c9a7f2a4a5549917f23e5
Michał Mieszczak <michal@mieszczak.com.pl>
2023-12-08 15:09:10 +0100

    Day 8 - part 1

---
+++ 2023/python/aoc/day08_haunted_wasteland/part1.py
@@ -0,0 +1,67 @@
+import io
+from functools import partial, reduce
+from itertools import cycle, takewhile
+from typing import Callable, Generator, Iterable, Mapping, NamedTuple, TypeVar
+
+
+class Directions(NamedTuple):
+    left: str
+    right: str
+
+
+def parse_line(line: str) -> tuple[str, Directions]:
+    match line.strip().split():
+        # AAA = (BBB, CCC)
+        case [location, "=", left, right]:
+            return location, Directions(
+                left.removeprefix("(").removesuffix(","),
+                right.removesuffix(")"),
+            )
+        case _:
+            raise ValueError(f"Invalid line: {line}")
+
+
+def parse_map(data: Iterable[str]) -> Mapping[str, Directions]:
+    m = list(map(parse_line, data))
+    return {k: v for k, v in m}
+
+
+def make_step(map_: Mapping[str, Directions], position: str, step: str) -> str:
+    current = map_[position]
+    return current.left if step == "L" else current.right
+
+
+T = TypeVar("T")
+V = TypeVar("V")
+
+
+def pass_map(
+    predicate: Callable[[T, V], T],
+    iterable: Iterable[V],
+    initial: T,
+) -> Generator[T, None, None]:
+    """Map function on elements of iterable,
+    additionally passing result of last iteration as a first argument
+    """
+    acc = initial
+    for element in iterable:
+        acc = predicate(acc, element)
+        yield acc
+
+
+def main(data: io.TextIOWrapper) -> int:
+    steps = data.readline().strip()
+    data.readline()
+    make_step_ = partial(make_step, parse_map(data))
+    return reduce(
+        lambda acc, _: acc + 1,
+        takewhile(
+            lambda x: x != "ZZZ",
+            pass_map(
+                lambda position, step: make_step_(position, step),
+                cycle(steps),
+                "AAA",
+            ),
+        ),
+        1,
+    )
