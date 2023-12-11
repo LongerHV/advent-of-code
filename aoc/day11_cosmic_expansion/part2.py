0c044e4deddedcd89ae75c33233c30432087a454
Michał Mieszczak <michal@mieszczak.com.pl>
2023-12-11 17:15:37 +0100

    Day 11 - part 2

--- aoc/day11_cosmic_expansion/part2.py
+++ aoc/day11_cosmic_expansion/part2.py
@@ -1,5 +1,43 @@
 import io
+from functools import cache, partial
+from itertools import combinations
+
+from .part1 import find_empty_rows, find_galaxies, transposed
 
 
-def main(data: io.TextIOWrapper) -> int:
-    raise NotImplementedError()
+@cache
+def range_set(start: int, stop: int) -> frozenset[int]:
+    return frozenset(range(start, stop))
+
+
+def distance(
+    a: tuple[int, int],
+    b: tuple[int, int],
+    empty_rows: set[int],
+    empty_cols: set[int],
+    spacing: int,
+) -> int:
+    x0, x1 = sorted((a[0], b[0]))
+    y0, y1 = sorted((a[1], b[1]))
+    return (
+        (x1 - x0)
+        + (y1 - y0)
+        + ((spacing - 1) * len(empty_rows & range_set(x0, x1)))
+        + ((spacing - 1) * len(empty_cols & range_set(y0, y1)))
+    )
+
+
+def main(data: io.TextIOWrapper, spacing: int = 1000000) -> int:
+    skymap = list(map(str.strip, data.readlines()))
+    distance_ = partial(
+        distance,
+        empty_rows=find_empty_rows(skymap),
+        empty_cols=find_empty_rows(transposed(skymap)),
+        spacing=spacing,
+    )
+    return sum(
+        map(
+            lambda x: distance_(*x),
+            combinations(find_galaxies(skymap), 2),
+        )
+    )
--- aoc/day11_cosmic_expansion/test_cosmic_expansion.py
+++ aoc/day11_cosmic_expansion/test_cosmic_expansion.py
@@ -4,18 +4,20 @@ from typing import Callable
 
 import pytest
 
-from . import part1  # , part2
+from . import part1, part2
 
 
 @pytest.mark.parametrize(
-    ["func", "file", "result"],
+    ["func", "file", "result", "args"],
     [
-        [part1.main, "example.txt", 374],
-        [part1.main, "input.txt", 9947476],
-        # [part2.main, "example.txt", 0],
-        # [part2.main, "input.txt", 0],
+        [part1.main, "example.txt", 374, []],
+        [part1.main, "input.txt", 9947476, []],
+        [part2.main, "example.txt", 8410, [100]],
+        [part2.main, "input.txt", 519939907614, []],
     ],
 )
-def test_cosmic_expansion(func: Callable[[io.TextIOWrapper], int], file: str, result: int):
+def test_cosmic_expansion(
+    func: Callable[[io.TextIOWrapper], int], file: str, result: int, args: list
+):
     with (Path(__file__).parent / "input" / file).open("r") as f:
-        assert func(f) == result
+        assert func(f, *args) == result
