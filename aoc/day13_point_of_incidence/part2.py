f65419f79d70120f816e59bd9f47dcef423cf08a
Michał Mieszczak <michal@mieszczak.com.pl>
2023-12-14 18:23:10 +0100

    Day 13 - part 2

--- aoc/day13_point_of_incidence/part2.py
+++ aoc/day13_point_of_incidence/part2.py
@@ -1,5 +1,38 @@
 import io
 
+from .part1 import parse_input, transposed
+
+
+def is_exactly_one_off(upper: list[str], lower: list[str]) -> bool:
+    min_length = min(len(upper), len(lower))
+    different = 0
+    for line_a, line_b in zip(list(reversed(upper))[:min_length], lower[:min_length]):
+        for char_a, char_b in zip(line_a, line_b):
+            if different > 1:
+                return False
+            if char_a == char_b:
+                continue
+            different += 1
+    return different == 1
+
+
+def count_rows_above_reflection(mirror: list[str], middlepoint: int | None = None) -> int:
+    if middlepoint == 0:
+        return 0
+    middlepoint = middlepoint or len(mirror) - 1
+    upper, lower = mirror[:middlepoint], mirror[middlepoint:]
+    if is_exactly_one_off(upper, lower):
+        return middlepoint
+    return count_rows_above_reflection(mirror, middlepoint - 1)
+
+
+def process_mirror(mirror: list[str]) -> int:
+    if line_count := count_rows_above_reflection(mirror):
+        return 100 * line_count
+    if line_count := count_rows_above_reflection(list(transposed(mirror))):
+        return line_count
+    return 0
+
 
 def main(data: io.TextIOWrapper) -> int:
-    raise NotImplementedError
+    return sum(map(process_mirror, parse_input(data)))
--- aoc/day13_point_of_incidence/test_point_of_incidence.py
+++ aoc/day13_point_of_incidence/test_point_of_incidence.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 import pytest
 
-from . import part1  # , part2
+from . import part1, part2
 
 
 @pytest.mark.parametrize(
@@ -12,8 +12,8 @@ from . import part1  # , part2
     [
         [part1.main, "example.txt", 405],
         [part1.main, "input.txt", 36041],
-        # [part2.main, "example.txt", 400],
-        # [part2.main, "input.txt", 0],
+        [part2.main, "example.txt", 400],
+        [part2.main, "input.txt", 35915],
     ],
 )
 def test_point_of_incidence(func: Callable[[io.TextIOWrapper], int], file: str, result: int):
